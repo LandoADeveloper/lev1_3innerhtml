6063958ff77216d910974fe21de4a85475f8f19e
LandoADeveloper <landom2222@gmail.com>
2023-01-04 13:38:18 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+let gallery = document.getElementById("gallery")
+
+gallery.innerHTML += "<figure><img src=\"https://images.unsplash.com/photo-1610053583122-74d4414ae49a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8b3dsc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60\"><figcaption>Fig1.</figcaption></figure>"
+gallery.innerHTML += "<figure><img src=\"https://images.unsplash.com/photo-1610053583122-74d4414ae49a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8b3dsc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60\"><figcaption>Fig2.</figcaption></figure>"
+gallery.innerHTML += "<figure><img src=\"https://images.unsplash.com/photo-1610053583122-74d4414ae49a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8b3dsc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60\"><figcaption>Fig3.</figcaption></figure>"
